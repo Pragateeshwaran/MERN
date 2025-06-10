1e91b23d0208e0d3bf8deeb2f71047617f7378cf
Pragateeshwaran <geniuspekka1808@gmail.com>
2025-06-10 12:16:32 +0530

    Mern Practice lessons formatted (#1)

--- revise/node/url.js
+++
@@ -1,8 +0,0 @@
-const url = require('url')
-const link = new URL("https://www.github.com/prgateeshwaran")
-
-console.log(link.protocol)
-console.log(link.pathname)
-console.log(link.hostname)
-console.log(link.port || 'default')
-console.log(link.origin)
